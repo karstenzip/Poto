fcd0ea453924a96a589481dc1d0e6d8ae7b04ef9
codeman1o1 <70335544+codeman1o1@users.noreply.github.com>
2025-07-31 15:07:07 +0200

    refactor: ♻️ Migrate to Nuxt 4

--- website/eslint.config.mjs
+++ website/eslint.config.mjs
@@ -1,5 +1,5 @@
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended"
-import withNuxt from "./.nuxt/eslint.config.mjs"
+import withNuxt from ".nuxt/eslint.config.mjs"
 
 export default withNuxt(eslintPluginPrettier, {
 	rules: {
